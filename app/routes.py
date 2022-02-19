319ac25bf02511f98c8489c959e8f125f95a782e
T-21B <li395jason@hotmail.com>
2022-02-19 10:52:07 -0500

    I commited again(still not to a relationship) working authentication + register system, need HTLM to redirect to

--- app/routes.py
+++ app/routes.py
@@ -7,26 +7,49 @@ from flask import (
 from json import *
 from flask import render_template
 from argon2 import PasswordHasher
+from werkzeug.utils import redirect
 
 bp = Blueprint('index', __name__, url_prefix='/')
+hasher = PasswordHasher()
+
 
 @bp.route("/auth/", methods=["POST", "GET"])
 def auth():
     if request.method == "POST":
-        hasher = PasswordHasher()
-        DB_pre = open("app/static/passwords.json", "r")
-        DB = json.loads(DB_pre.read())
+        with open("app/static/passwords.json", "r") as DB_pre:
+            DB = json.loads(DB_pre.read())
+        form_user = request.form["user"]
+        form_pass = request.form["pass"]
+        if form_user == "" or form_pass == "":
+            return "FieldIsNone"
         try:
-            hasher.verify(DB[request.form["user"]], request.form["pass"])
+            hasher.verify(DB[form_user], form_pass)
         except VerifyMismatchError:
-            g.soemthing = "asd"
             return "WrongPassword"
         else:
-            return "RightPassword"
+            return "RightPassword" # Redirect to initial location?
     else:
         return "<h1>ASDASDASD</h1>"
 
 
+@bp.route("/register/", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        form_IGN = request.form["user"]
+        form_Pass = hasher.hash(request.form["pass"])
+        with open("app/static/passwords.json", "r") as file:
+            data = json.load(file)
+            if form_IGN not in data:
+                data[form_IGN] = form_Pass
+            else:
+                data["ERROR"] = "Errored123"
+        with open("app/static/passwords.json", "w") as file:
+            json.dump(data, file, indent=4)
+        return redirect("/") # Redirect to initial location
+    else:
+        return "<h1>Resgister form</h1>"
+
+
 @bp.route("", methods=["GET"])
 def index():
-    return g.something
+    return "OKAY"
