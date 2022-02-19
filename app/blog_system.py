0fd7bfa11b67383b34f77d816be397fabe5fd4d0
Vincent <vincentvibe3@gmail.com>
2022-02-19 12:15:13 -0500

    add blog list

--- app/blog_system.py
+++ app/blog_system.py
@@ -9,11 +9,18 @@ import markdown
 bp = Blueprint('blog', __name__, url_prefix='/blog')
 
 @bp.route("/<blog>", methods=["GET"])
-def index(blog):
+def blog(blog):
+    print("hello")
     with open(f"app/content/{blog}.md", "r") as blogtext:
         g.blogHtml = markdown.markdown(blogtext.read())
     return render_template("blog.html")
 
+@bp.route("/", methods=["GET"])
+def index():
+    print("list")
+    with open(f"app/content/index.json", "r") as blogtext:
+        g.posts = json.loads(blogtext.read())
+    return render_template("blog_list.html")
 
 @bp.route("/submit", methods=["GET", "POST"])
 def submit():
@@ -22,11 +29,12 @@ def submit():
     elif request.method == "POST":
         print(request.form)
         timestamp = int(time.time())
+        title = request.form["titleinput"]
         with open(f"app/content/{timestamp}.md", "w") as f:
-            f.write(request.form["textinput"])
+            f.write(f"# {title}\n{request.form['textinput']}")
         with open(f"app/content/index.json", "r") as f:
             index = json.loads(f.read())
         with open(f"app/content/index.json", "w") as f:
-            index.append(timestamp)
+            index.append({"title":title, "id":str(timestamp)})
             f.write(json.dumps(index, indent=4))
         return render_template("blog_success.html")
